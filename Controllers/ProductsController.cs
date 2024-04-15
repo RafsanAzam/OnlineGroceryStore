d10064b245b967412c003595ee5e97f37c4af96c
RafsanAzam <azam.rafsan@gmail.com>
2024-04-15 10:26:36 +1000

    Update ProductsController.cs
    Post Method for Edit product added.

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using OnlineGroceryStore.Models;
 using OnlineGroceryStore.Models.Data;
 
 namespace OnlineGroceryStore.Controllers
@@ -37,5 +38,62 @@ namespace OnlineGroceryStore.Controllers
 
             return View(product);
         }
+
+        // GET: URL -- eg. Products/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FindAsync(id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Products.Any(p => p.ProductId == id);
+        }
+
+        // POST: URL -- eg. Products/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit (int id, [Bind("ProductId, Name, Price")] Product product)
+        {
+            if(id != product.ProductId)
+            {
+                return NotFound();
+            }
+
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+
+                catch (DbUpdateConcurrencyException)
+                {
+                    if(!ProductExists(product.ProductId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index)); //Using nameof(Index) enhances refactoring safety and typo prevention.
+
+            }
+            return View(product);
+        }   
     }
 }
