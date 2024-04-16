c8f215ce7c1966c282e480a182610dd61d2c0ab6
RafsanAzam <azam.rafsan@gmail.com>
2024-04-16 12:17:10 +1000

    Update Category.cs
    updated Category model.

--- Models/Category.cs
+++ Models/Category.cs
@@ -4,9 +4,13 @@ namespace OnlineGroceryStore.Models
 {
     public class Category
     {
-        [Key]
         public int CategoryId { get; set; }
-        [Required]
         public string Name { get; set; }
+        public int? ParentCategoryId { get; set; } // Nullable for top-level categories
+
+        // Navigation properties
+        public Category ParentCategory { get; set; }
+        public List<Category> SubCategories { get; set; }
+        public List<Product> Products { get; set; }
     }
 }
