c6b74d8d82d95048154a4f3ff3cf2134063b480f
RafsanAzam <azam.rafsan@gmail.com>
2024-04-16 12:25:38 +1000

    Create OrderDetail.cs
    Created OrderDetail Model to handle many to many relationship between Order and Products.

---
+++ Models/OrderDetail.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineGroceryStore.Models
+{
+    public class OrderDetail
+    {
+        public int OrderDetailId { get; set; }
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public Order Order { get; set; }
+        public Product Product { get; set; }
+    }
+}
