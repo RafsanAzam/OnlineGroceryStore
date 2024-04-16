ce8b563f0a66b06d5ecc53d5380a892fe06a7acc
RafsanAzam <azam.rafsan@gmail.com>
2024-04-16 14:07:35 +1000

    Update SubCategory.cs

--- Models/SubCategory.cs
+++ Models/SubCategory.cs
@@ -4,8 +4,8 @@
     {
         public int SubCategoryId { get; set; }
         public string Name { get; set; }
-        public int ParentCategoryId { get; set; }
-        public Category ParentCategory { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
         public List<Product> Products { get; set; }  // Products directly associated with this subcategory
     }
 }
