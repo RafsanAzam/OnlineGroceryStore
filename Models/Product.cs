648a6e242d37bf058fbedd381aac482a35e9a61b
RafsanAzam <azam.rafsan@gmail.com>
2024-04-16 12:13:26 +1000

    Update Product.cs
    Updated Product Model.

--- Models/Product.cs
+++ Models/Product.cs
@@ -8,5 +8,9 @@ namespace OnlineGroceryStore.Models
         public int ProductId { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public int StockQuantity { get; set; }
+        public Category Category { get; set; }
     }
 }
