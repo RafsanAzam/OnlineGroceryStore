acbc61edef67f5d723c53d6fe835de18495e1808
RafsanAzam <azam.rafsan@gmail.com>
2024-04-16 15:08:54 +1000

    database updated

---
+++ Migrations/20240416042603_CategorySubCategorySeparated.cs
@@ -0,0 +1,370 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace OnlineGroceryStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class CategorySubCategorySeparated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Users_UserId",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "StockQuantity",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SubCategoryId",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Orders",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetails",
+                columns: table => new
+                {
+                    OrderDetailId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderId = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetails", x => x.OrderDetailId);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "OrderId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SubCategories",
+                columns: table => new
+                {
+                    SubCategoryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubCategories", x => x.SubCategoryId);
+                    table.ForeignKey(
+                        name: "FK_SubCategories_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Fruits" },
+                    { 2, "Vegetables" },
+                    { 3, "Dairy" },
+                    { 4, "Beverages" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1,
+                columns: new[] { "CategoryId", "Description", "Name", "StockQuantity", "SubCategoryId" },
+                values: new object[] { 1, "Fresh Oranges", "Orange", 150, 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2,
+                columns: new[] { "CategoryId", "Description", "Name", "Price", "StockQuantity", "SubCategoryId" },
+                values: new object[] { 2, "Organic Carrots", "Carrot", 0.20000000000000001, 200, 2 });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3,
+                columns: new[] { "CategoryId", "Description", "Name", "Price", "StockQuantity", "SubCategoryId" },
+                values: new object[] { 2, "Fresh Spinach", "Spinach", 1.0, 100, 3 });
+
+            migrationBuilder.InsertData(
+                table: "SubCategories",
+                columns: new[] { "SubCategoryId", "CategoryId", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "Citrus" },
+                    { 2, 2, "Root" },
+                    { 3, 2, "Leafy Greens" },
+                    { 4, 3, "Milk" },
+                    { 5, 3, "Cheese" },
+                    { 6, 4, "Sodas" },
+                    { 7, 4, "Juices" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "CategoryId", "Description", "Name", "Price", "StockQuantity", "SubCategoryId" },
+                values: new object[,]
+                {
+                    { 4, 3, "Creamy Whole Milk", "Whole Milk", 0.89000000000000001, 300, 4 },
+                    { 5, 3, "Aged Cheddar Cheese", "Cheddar Cheese", 2.5, 95, 5 },
+                    { 6, 4, "Classic Coca Cola", "Coca Cola", 1.25, 500, 6 },
+                    { 7, 4, "Pure Apple Juice", "Apple Juice", 1.5, 150, 7 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_SubCategoryId",
+                table: "Products",
+                column: "SubCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_OrderId",
+                table: "OrderDetails",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_ProductId",
+                table: "OrderDetails",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubCategories_CategoryId",
+                table: "SubCategories",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Users_UserId",
+                table: "Orders",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_SubCategories_SubCategoryId",
+                table: "Products",
+                column: "SubCategoryId",
+                principalTable: "SubCategories",
+                principalColumn: "SubCategoryId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Users_UserId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_SubCategories_SubCategoryId",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "OrderDetails");
+
+            migrationBuilder.DropTable(
+                name: "SubCategories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_SubCategoryId",
+                table: "Products");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 4);
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "StockQuantity",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "SubCategoryId",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1,
+                column: "Name",
+                value: "Apple");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2,
+                columns: new[] { "Name", "Price" },
+                values: new object[] { "Banana", 0.29999999999999999 });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3,
+                columns: new[] { "Name", "Price" },
+                values: new object[] { "Carrot", 0.20000000000000001 });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Users_UserId",
+                table: "Orders",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
