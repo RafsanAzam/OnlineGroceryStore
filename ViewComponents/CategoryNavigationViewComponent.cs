ef72cf90e6e0ce1e07a26245e03475663203edec
RafsanAzam <azam.rafsan@gmail.com>
2024-04-17 14:23:20 +1000

    Create CategoryNavigationViewComponent.cs
    This component will allow users to navigate through categories and their subcategories.

---
+++ ViewComponents/CategoryNavigationViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineGroceryStore.Models.Data;
+
+namespace OnlineGroceryStore.ViewComponents
+{
+    public class CategoryNavigationViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryNavigationViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = await _context.Categories.Include(c => c.SubCategories).ToListAsync();
+            return View(categories);
+        }
+    }
+}
