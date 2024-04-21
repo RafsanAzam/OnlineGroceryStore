d1441f8a463b69fdcb43e7835ea0f76d5eab9d27
RafsanAzam <azam.rafsan@gmail.com>
2024-04-21 10:50:47 +1000

    updated for accordion display

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
