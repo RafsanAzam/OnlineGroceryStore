3e74729ccfd6055d9b2423b985dc02782c09225c
RafsanAzam <azam.rafsan@gmail.com>
2024-04-25 19:32:36 +1000

    Update Program.cs
    Registered CartService inside Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using OnlineGroceryStore.Models.Data;
+using OnlineGroceryStore.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options=> options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Register CartService
+builder.Services.AddScoped<CartService>();
 
 var app = builder.Build();
 
